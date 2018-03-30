02255548a0eb6932b8a832d0ecc4f5c0b108e4a0
agDevelop <andrewnikolaevich.2014@gmail.com>
2018-03-30 15:34:51 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enums
+{
+    enum Number
+    {
+        Zero,
+        One,
+        Two,
+        Three
+    }
+
+    class Program
+    {
+        private static Number myDigit = Number.One;
+
+        static void Main(string[] args)
+        {
+            Number digit = Number.Two;
+
+            Console.WriteLine(digit);
+
+            Console.WriteLine((int)digit);
+
+            Console.WriteLine(new string('-', 10));
+
+            Console.WriteLine(Number.Three);
+
+            Console.WriteLine((int)Number.Three);
+
+            Console.WriteLine((int)myDigit);
+
+            Console.ReadKey();
+        }
+    }
+}
